8aade2cef99bcb032ca60f8ba654a5beb60491ba
Adriano Freitas <archl0rd@adrianofreitas.me>
2017-04-13 06:44:13 +0000

    lista atualizada

--- configurar.sh
+++ configurar.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 # archl0rd - adrianofreitas.me
-# 31/03/2017
-#sudo ./criar-usuario.sh
- apt-get update
- cd script
- sh *.sh
+# 14/04/2017
+apt-get update
+cd script
+sh java8.sh
+sh atom.sh
+sh python3.6.sh
+sh  codeblocks.sh
+sh  ghc.sh
+sh  latex.sh
+sh  rede.sh
+sh  eclipse-neon.sh
+sh  git-kraken.sh
+sh  multimidia.sh
+sh  editor-texto.sh
+sh  grafico.sh
+sh  netbeans.sh
+sh  erlang.sh
+sh  intelliJ.sh
+sh  pip3.sh
+sh  virtualbox.sh
+sh  escritorio.sh
+sh  internet.sh
+sh  programas-basicos.sh
+sh  wireshark.sh
+cd ..
