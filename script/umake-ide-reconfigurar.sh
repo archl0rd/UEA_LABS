d1c08fc4fbaa41ab0773d1f08cc288515cc14f73
Adriano Freitas <archl0rd@adrianofreitas.me>
2017-04-14 08:22:36 +0000

    modificado

---
+++ script/umake-ide-reconfigurar.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#archl0rd - adrianofreitas.me
+# 05/04/17
+## Script para reconfigurar os programas instalados com o umake
+## pois os mesmos não estão acessíveis para outros usuários
+
+## Faça login na conta onde foi instalado os programas
+## Abra um terminal, e rode os comandos abaixos
+cp -R ~/.local/share/umake/ide /opt
+
+
+## Agora acesse os demais usuários e sigas as instruções
+
+## idea
+## Abra um terminal, e rode o comando abaixo
+# /opt/ide/idea/bin/idea.sh
+## se o ataalho não for criado, clique com o botão direito no ícone do programa na barra lateral
+## e adicione-o ao dash ou lançador
+
+
+## netbeans
+## Abra um terminal, e rode o comando abaixo
+# /opt/ide/netbeans/bin/netbeans
+## se o ataalho não for criado, clique com o botão direito no ícone do programa na barra lateral
+## e adicione-o ao dash ou lançador
+
+
+## pycharm
+## Abra um terminal, e rode o comando abaixo
+# /opt/ide/pycharm/bin/pycharm.sh
+## se o ataalho não for criado, clique com o botão direito no ícone do programa na barra lateral
+## e adicione-o ao dash ou lançador
+
+
+
